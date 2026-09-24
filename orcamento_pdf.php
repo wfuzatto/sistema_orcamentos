04a4c80e4774bfbe5d55570c5a5a2ded077f33f4
wfuzatto <shockwave_acb@hotmail.com>
2026-09-24 16:03:10 -0300

    fix: expect bundled Dompdf package

--- orcamento_pdf.php
+++ orcamento_pdf.php
@@ -1,4 +1,4 @@
 <?php
 require 'includes/orcamento_render.php';$id=(int)($_GET['id']??0);$o=orcamentoData($pdo,$id);$autoload=__DIR__.'/vendor/autoload.php';
-if(!file_exists($autoload))exit('Dompdf ainda não instalado. Execute "composer install" na pasta do projeto. Você pode usar a opção Imprimir como PDF enquanto isso.');
+if(!file_exists($autoload))exit('Pacote Dompdf ausente ou incompleto nesta cópia da aplicação. Atualize o projeto pelo GitHub.');
 require $autoload;$options=new Dompdf\Options();$options->set('isRemoteEnabled',true);$options->set('defaultFont','DejaVu Sans');$pdf=new Dompdf\Dompdf($options);$pdf->loadHtml(renderOrcamento($o,true),'UTF-8');$pdf->setPaper('A4','portrait');$pdf->render();$pdf->stream('orcamento-'.str_pad((string)$o['numero'],5,'0',STR_PAD_LEFT).'.pdf',['Attachment'=>false]);
